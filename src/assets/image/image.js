6e11c724ca7b575c427b2390663966e4a28c3160
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-03-10 03:33:18 +0600

    add images

---
+++ src/assets/image/image.js
@@ -0,0 +1,26 @@
+import logo from './logo.png'
+
+
+import burger from './burger.jpg'
+import cake from './cake.jpg'
+import dessent from './dessent.jpg'
+import fizza from './fizza.jpg'
+import meetboll from './meet boll.jpg'
+import salad from './salad.jpg'
+import steak from './steak.jpg'
+import sushi1 from './sushi-1.jpg'
+import sushi2 from './sushi-2.jpg'
+
+
+export const images = {
+    logo,
+  burger,
+  cake,
+  dessent,
+  fizza,
+  meetboll,
+  salad,
+  steak,
+  sushi1,
+  sushi2
+};
