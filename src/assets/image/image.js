686a8949aed484a33f7c57bb5ccefaeb10845fcb
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-03-11 05:19:50 +0600

    add all foods image in image folder

--- src/assets/image/image.js
+++ src/assets/image/image.js
@@ -1,26 +1,422 @@
-import logo from './logo.png'
+// navbar image
+import logo from "./logo.png";
+import basket from "./basket icon.png";
+import cross from "./cross-icon.png";
+import plus from "./Plus_icon.png";
 
+// cover images
+import cover_1 from "./Cover-1.png";
+import cover_2 from "./Cover-2.png";
+import cover_3 from "./Cover-3.png";
 
-import burger from './burger.jpg'
-import cake from './cake.jpg'
-import dessent from './dessent.jpg'
-import fizza from './fizza.jpg'
-import meetboll from './meet boll.jpg'
-import salad from './salad.jpg'
-import steak from './steak.jpg'
-import sushi1 from './sushi-1.jpg'
-import sushi2 from './sushi-2.jpg'
+// category images
+import cake from "./Cake-icon.png";
+import dessert from "./Dessert-icon.jpg";
+import noodles from "./Noodles-icon.jpg";
+import Pasta from "./Pasta-icon.jpg";
+import salad from "./Salad-icon.png";
+import sandwich from "./Sandwich-icon.jpg";
+import sushi from "./sushi-icon.jpg";
 
+// foods images
+import cake_1 from "./Cake-1.jpg";
+import cake_2 from "./Cake-2.jpg";
+import cake_3 from "./Cake-3.jpg";
+import cake_4 from "./Cake-4.jpg";
+import cake_5 from "./Cake-5.jpg";
+import cake_6 from "./Cake-6.jpg";
+import cake_7 from "./Cake-7jpg";
+import cake_8 from "./Cake-8.jpg";
+import cake_9 from "./Cake-9.jpg";
+
+import dessert_1 from "./Dessert-1.jpg";
+import dessert_2 from "./Dessert-2.jpg";
+import dessert_3 from "./Dessert-3.jpg";
+import dessert_4 from "./Dessert-4.jpg";
+import dessert_5 from "./Dessert-5.jpg";
+import dessert_6 from "./Dessert-6.jpg";
+import dessert_7 from "./Dessert-7.jpg";
+import dessert_8 from "./Dessert-8.jpg";
+import dessert_9 from "./Dessert-9.jpg";
+import dessert_10 from "./Dessert-10.jpg";
+import dessert_11 from "./Dessert-11.jpg";
+
+import noodles_1 from "./Noodles-1.png";
+import noodles_2 from "./Noodles-2.png";
+import noodles_3 from "./Noodles-3.png";
+import noodles_4 from "./Noodles-4.png";
+import noodles_5 from "./Noodles-5.png";
+import noodles_6 from "./Noodles-6.png";
+import noodles_7 from "./Noodles-7.png";
+import noodles_8 from "./Noodles-8.png";
+import noodles_9 from "./Noodles-9.png";
+import noodles_10 from "./Noodles-10.png";
+import noodles_11 from "./Noodles-11.png";
+
+import pasta_1 from "./Pasta-1.jpg";
+import pasta_2 from "./Pasta-2.jpg";
+import pasta_3 from "./Pasta-3.jpg";
+import pasta_4 from "./Pasta-4.jpg";
+import pasta_5 from "./Pasta-5.jpg";
+import pasta_6 from "./Pasta-6.jpg";
+import pasta_7 from "./Pasta-7.jpg";
+import pasta_8 from "./Pasta-8.jpg";
+import pasta_9 from "./Pasta-9.jpg";
+import pasta_10 from "./Pasta-10.jpg";
+import pasta_11 from "./Pasta-11.jpg";
+
+import salad_1 from "./salad-1.jpg";
+import salad_2 from "./salad-2.jpg";
+import salad_3 from "./salad-3.jpg";
+import salad_4 from "./salad-4.jpg";
+import salad_5 from "./salad-5.jpg";
+import salad_6 from "./salad-6.jpg";
+import salad_7 from "./salad-7.jpg";
+import salad_8 from "./salad-8.jpg";
+import salad_9 from "./salad-9.jpg";
+import salad_10 from "./salad-10.jpg";
+import salad_11 from "./salad-11.jpg";
+
+import sandwich_1 from "./Sandwich-1.jpg";
+import sandwich_2 from "./Sandwich-2.jpg";
+import sandwich_3 from "./Sandwich-3.jpg";
+import sandwich_4 from "./Sandwich-4.jpg";
+import sandwich_5 from "./Sandwich-5.jpg";
+import sandwich_6 from "./Sandwich-6.jpg";
+import sandwich_7 from "./Sandwich-7.jpg";
+import sandwich_8 from "./Sandwich-8.jpg";
+import sandwich_9 from "./Sandwich-9.jpg";
+
+import sushi_1 from "./sushi-1.jpg";
+import sushi_2 from "./sushi-2.jpg";
+import sushi_3 from "./sushi-3.jpg";
+import sushi_4 from "./sushi-4.jpg";
+import sushi_5 from "./sushi-5.jpg";
+import sushi_6 from "./sushi-6.jpg";
+import sushi_7 from "./sushi-7.jpg";
+import sushi_8 from "./sushi-8.jpg";
+import sushi_9 from "./sushi-9.jpg";
+import sushi_10 from "./sushi-10.jpg";
+import sushi_11 from "./sushi-11.jpg";
+import sushi_12 from "./sushi-12.jpg";
+import sushi_13 from "./sushi-13.jpg";
+import sushi_14 from "./sushi-14.jpg";
+import sushi_15 from "./sushi-15.jpg";
+import sushi_16 from "./sushi-16.jpg";
 
 export const images = {
-    logo,
-  burger,
-  cake,
-  dessent,
-  fizza,
-  meetboll,
-  salad,
-  steak,
-  sushi1,
-  sushi2
+  logo,
+  basket,
+  cover_1,
+  cover_2,
+  cover_3,
+  cross,
+  plus,
 };
+
+export const category = [
+  {
+    cate_name: "Sushi",
+    cata_img: sushi,
+  },
+  {
+    cate_name: "Salad",
+    cata_img: salad,
+  },
+  {
+    cate_name: "Noodles",
+    cata_img: noodles,
+  },
+  {
+    cate_name: "Dessert",
+    cata_img: dessert,
+  },
+  {
+    cate_name: "Cake",
+    cata_img: cake,
+  },
+  {
+    cate_name: "Sandwich",
+    cata_img: sandwich,
+  },
+  {
+    cate_name: "Pasta",
+    cata_img: Pasta,
+  },
+];
+
+export const dish = [
+  // Sushi food items
+  {
+    _id: "1",
+    name: "Sushi",
+    image: sushi_1,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "2",
+    name: "Sushi",
+    image: sushi_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "3",
+    name: "Sushi",
+    image: sushi_3,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "4",
+    name: "Sushi",
+    image: sushi_4,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "5",
+    name: "Sushi",
+    image: sushi_5,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "6",
+    name: "Sushi",
+    image: sushi_6,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "7",
+    name: "Sushi",
+    image: sushi_7,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "8",
+    name: "Sushi",
+    image: sushi_8,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "9",
+    name: "Sushi",
+    image: sushi_9,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "10",
+    name: "Sushi",
+    image: sushi_10,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "11",
+    name: "Sushi",
+    image: sushi_11,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "12",
+    name: "Sushi",
+    image: sushi_12,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "13",
+    name: "Sushi",
+    image: sushi_13,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "14",
+    name: "Sushi",
+    image: sushi_14,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "15",
+    name: "Sushi",
+    image: sushi_15,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  {
+    _id: "16",
+    name: "Sushi",
+    image: sushi_16,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "sushi",
+  },
+  // Salad food items
+  {
+    _id: "17",
+    name: "Salad",
+    image: salad_1,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "18",
+    name: "Salad",
+    image: salad_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "19",
+    name: "Salad",
+    image: salad_3,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "20",
+    name: "Salsd",
+    image: salad_4,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "21",
+    name: "Salad",
+    image: salad_5,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "22",
+    name: "salad",
+    image: salad_6,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "23",
+    name: "salad",
+    image: salad_7,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "24",
+    name: "Salad",
+    image: salad_8,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  // noodles food items
+  {
+    _id: "25",
+    name: "Salad",
+    image: salad_9,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "26",
+    name: "Noodles",
+    image: noodles_1,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "noodles",
+  },
+  {
+    _id: "27",
+    name: "Noodles",
+    image: noodles_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "noodles",
+  },
+  {
+    _id: "28",
+    name: "Nooduls",
+    image: noodles_3,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "noodles",
+  },
+  {
+    _id: "29",
+    name: "Noodles",
+    image: noodles_4,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "noodles",
+  },
+  {
+    _id: "30",
+    name: "Noodles",
+    image: noodles_5,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "noodles",
+  },
+];
