031c1e7aed3ae4ceeb368ace209d1fd41de69071
[Meharazul islam] <[itsmeharazz@gmail.com]>
2025-03-12 03:05:48 +0600

    add multipage option

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import Nav from './components/navbar/nav'
-const App = () => {
-  return <div className='app'>
-    <Nav />
-  </div>
-}
+import React from "react";
+import Nav from "./components/navbar/nav";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import PlaceOrder from "./pages/placeOrder/PlaceOrder";
 
-export default App
+const App = () => {
+  return (
+    <div className='app'>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
